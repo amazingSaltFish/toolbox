19687c744a2d1ed1b8c8a8071f68e0e1b6f94584
wmh <wmhdaydayup@163.com>
2021-06-28 22:17:45 +0800

    fix sparkSession is null

--- spark-with-datasource/src/test/java/App.java
+++ spark-with-datasource/src/test/java/App.java
@@ -9,7 +9,6 @@ import org.apache.spark.sql.SparkSession;
 public class App {
     public static void main(String[] args) {
         SparkSession spark = SparkSession.builder().master("local").getOrCreate();
-        System.out.println(spark.read().text("hdfs://10.122.3.35:8020/data_from_sc/").count());
         spark.stop();
     }
 }
